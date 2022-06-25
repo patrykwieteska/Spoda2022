ad179a517054addfbf49c3e79c77b4b69c6301f2
patrykwieteska <patrykwieteskapw@gmail.com>
2022-06-25 15:19:45 +0200

    Initial commit

---
+++ src/test/java/pl/pw/spoda/Spoda/SpodaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pw.spoda.Spoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
